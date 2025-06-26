b38f0b5c7660b724618947915d25a2a7f502adb2
sid <sid@LAPTOP-NP7O1N5A.>
2025-06-25 23:48:19 -0700

    schitzo

---
+++ ring/map.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+/*
+
+K % capacity
+
+
+*/
+
+using namespace std;
+
+template <typename K, typename V>
+struct HashMap {
+  K key;
+  V val;
+  bool occupied;
+};
+
+struct slot {
+  int index;
+  int value;
+  bool occupied;
+};
+
+class Map {
+ public:
+  explicit Map(int capacity)
+      : capacity_{capacity},
+        table_(new slot[capacity]),
+        size_{0},
+        head_{0},
+        tail_{0} {}
+
+  bool isEmpty(int index) { return true; }
+
+  void insert(int key, int val) {
+    int index = key % this->capacity_;
+    if (isEmpty(index)) {
+      this->table_[index].value = val;
+      this->table_[index].index = index;
+    } else {
+      while (!isEmpty(index)) {
+        index = index + 1 % capacity_;
+      }
+      this->table_[index].value = val;
+      this->table_[index].index = index;
+    }
+  }
+
+ private:
+  slot *table_;
+  int size_;
+  int capacity_;
+  int head_;
+  int tail_;
+};
+
+int main(int argc, char *argv[]) { return 0; }
