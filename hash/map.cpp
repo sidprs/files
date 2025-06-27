e584544468d50d801e42201bd2ccf5e2a680e644
sid <sid@LAPTOP-NP7O1N5A.>
2025-06-26 23:03:46 -0700

    h

--- hash/map.cpp
+++ hash/map.cpp
@@ -17,7 +17,7 @@ struct HashMap {
 };
 
 struct slot {
-  int index;
+  int key;
   int value;
   bool occupied;
 };
@@ -29,21 +29,51 @@ class Map {
         table_(new slot[capacity]),
         size_{0},
         head_{0},
-        tail_{0} {}
+        tail_{0} {
+    for (int i = 0; i < capacity_; i++) {
+      this->table_[i].value = 0;
+      this->table_[i].key = 0;
+      this->table_[i].occupied = false;
+    }
+  }
 
-  bool isEmpty(int index) { return true; }
+  bool isEmpty(int index) { return !this->table_[index].occupied; }
+
+  void look_up(int key) {
+    int index = key % capacity_;
+    int start = index;
+
+    while (this->table_[index].occupied) {
+      if (this->table_[index].key == key) {
+        std::cout << " found " << index;
+        std::cout << " val " << this->table_[index].value << std::endl;
+        return;
+      } else {
+        index = (index + 1) % this->capacity_;
+      }
+      if (index == start) {
+        std::cout << "not found" << std::endl;
+        break;
+      }
+    }
+  }
 
   void insert(int key, int val) {
     int index = key % this->capacity_;
-    if (isEmpty(index)) {
-      this->table_[index].value = val;
-      this->table_[index].index = index;
-    } else {
-      while (!isEmpty(index)) {
-        index = index + 1 % this->capacity_;
-      }
-      this->table_[index].value = val;
-      this->table_[index].index = index;
+    while (!isEmpty(index)) {
+      index = (index + 1) % this->capacity_;
+    }
+    this->table_[index].value = val;
+    this->table_[index].key = key;
+    this->table_[index].occupied = true;
+  }
+
+  void print() {
+    for (int i = 0; i < this->capacity_; i++) {
+      std::cout << " index " << i;
+      // std::cout << " key " << this->table_[i].index;
+      std::cout << " key " << this->table_[i].key;
+      std::cout << " val " << this->table_[i].value << std::endl;
     }
   }
 
@@ -55,4 +85,14 @@ class Map {
   int tail_;
 };
 
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) {
+  Map r(5);
+  r.insert(4, 1);
+  r.insert(9, 99);
+  r.print();
+  r.look_up(4);  // Should print value 1
+  r.look_up(9);  // Should print value 99
+  r.look_up(8);  // Should print not found
+
+  return 0;
+}
