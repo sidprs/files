930119317b12e5945e2f72167ee783af991ff91d
sidprs <sidprasad2004@gmail.com>
2025-11-25 23:29:41 -0800

    added a simple web server in C, explains how client and server side sockets work

---
+++ webserver/src/client.c
@@ -0,0 +1,79 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
+/*
+
+
+    while (1) {
+        printf("You: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        
+        send(sock, buffer, strlen(buffer), 0);
+        
+        // Wait for server response
+        memset(buffer, 0, sizeof(buffer));
+        read(sock, buffer, sizeof(buffer));
+        printf("Server: %s\n", buffer);
+
+    }
+ */
+
+
+
+int main(int argc, char const* argv[])
+{
+    int status, valread, client_fd;
+    struct sockaddr_in serv_addr;
+    char* hello = "Hello from client";
+    char buffer[1024] = { 0 };
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary
+    // form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
+        <= 0) {
+        printf(
+            "\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if ((status
+         = connect(client_fd, (struct sockaddr*)&serv_addr,
+                   sizeof(serv_addr)))
+        < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+  
+    // subtract 1 for the null
+    // terminator at the end
+    //send(client_fd, hello, strlen(hello), 0);
+    //printf("Hello message sent\n");
+    
+    printf("You: ");
+    fgets(buffer, sizeof(buffer), stdin);  // Read from keyboard
+    
+    send(client_fd, buffer, strlen(buffer), 0);
+    
+    // STEP 5: Wait for server response
+    memset(buffer, 0, sizeof(buffer));  // Clear buffer
+    read(client_fd, buffer, sizeof(buffer) - 1);
+    printf("Server: %s\n", buffer);
+    valread = read(client_fd, buffer,
+                   1024 - 1); 
+    printf("%s\n", buffer);
+
+    // closing the connected socket
+    close(client_fd);
+    return 0;
+}
---
+++ webserver/src/server.c
@@ -0,0 +1,76 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
+
+
+int main(int argc, char *argv[]){
+    printf("hello world\n");
+
+    struct sockaddr_in address;
+
+    int server_fd, new_socket;
+    ssize_t valread;
+    int opt = 1;
+
+    socklen_t addrlen = sizeof(address);
+    char buffer[1024] = { 0 };
+    char* hello = "Hello from server";
+
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        // if server_fd is less than 0 then error 
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    int establish = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    if (establish != 0 ) {
+        perror("we not good\n");
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr*)&address,
+             sizeof(address))
+        < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket
+         = accept(server_fd, (struct sockaddr*)&address,
+                  &addrlen))
+        < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+  
+    // subtract 1 for the null
+    // terminator at the end
+    valread = read(new_socket, buffer,
+                   1024 - 1); 
+    printf("%s\n", buffer);
+    send(new_socket, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+
+    // closing the connected socket
+    close(new_socket);
+  
+    // closing the listening socket
+    close(server_fd);
+    printf("closing server_fd\n");
+    return 0;
+
+}
+
